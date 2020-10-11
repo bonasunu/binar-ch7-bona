cd6043d6f0b78b74123e1e51efe40954b11859a9
bonasunu <bonaventurasunu@gmail.com>
2020-10-11 21:17:09 +0700

    add pseudo-code for game logic

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -36,20 +36,26 @@ const createRoom = async (req, res) => {
 
 const fightRoom = async (req, res) => {
   const room_id = req.params.room
-  const room = await db.Room.findOne({where: {id: room_id}, include: [db.Log]})
-  if (!room) res.status(404).json({info: "Room doesn't exist. Please create new room first."})
+  const room = await db.Room.findOne({
+    where: { id: room_id },
+    include: [db.Log],
+  })
+  if (!room)
+    res
+      .status(404)
+      .json({ info: "Room doesn't exist. Please create new room first." })
 
-  let activeLog = room['Logs'].filter(i => i.is_active === true)
+  let activeLog = room['Logs'].filter((i) => i.is_active === true)
 
   // TODO Create helper to deactivate session
-  
+
   // check if room exist, then check active log
   //find a log, if log doesn't exist, then create it
   // if log, check empty session. if full, choose another room
   // find open session
   // play with 2 players
   // set is_active false if error happen
-  
+
   if (room && activeLog.length === 0) {
     const newLog = await db.Log.create({
       log_id: uuidv4(),
@@ -57,38 +63,54 @@ const fightRoom = async (req, res) => {
       winner: [],
       session: [req.user.id],
       is_active: true,
-      playing_date: new Date()
+      playing_date: new Date(),
     })
     activeLog.concat(newLog)
-    res.json({info: 'You joined this room'})
+    res.json({ info: 'You joined this room' })
   } else if (room && activeLog[0].session.length < 2) {
     // add player
     // if duplicate player, return error
     // if full, return error
     // else, success
-    const duplicateUser = await activeLog[0].session.some(i => i === req.user.id)
-    if (duplicateUser) res.status(202).json({info: 'You are in the room right now.'})
+    const duplicateUser = await activeLog[0].session.some(
+      (i) => i === req.user.id
+    )
+    if (duplicateUser)
+      res.status(202).json({ info: 'You are in the room right now.' })
     else if (!duplicateUser) {
       const session = activeLog[0].session.concat(req.user.id)
 
-      await db.Log.update({
-        session: session
-      }, {
-        where: {
-          log_id: activeLog[0].log_id
+      await db.Log.update(
+        {
+          session: session,
+        },
+        {
+          where: {
+            log_id: activeLog[0].log_id,
+          },
         }
-      })
+      )
 
       activeLog[0].session = session
-      res.json({info: 'You joined this room'})
+      res.json({ info: 'You joined this room' })
     }
   } else if (room && activeLog[0].session.length === 2) {
-    const duplicateUser = await activeLog[0].session.some(i => i === req.user.id)
-    if (duplicateUser) res.status(202).json({info: 'You are in the room right now.'})
-    else res.status(404).json({info: "Room is full. Please join other room or create new room."})
-  } else res.status(404).json({info: "Room is full. Please join other room or create new room."})
-  
+    const duplicateUser = await activeLog[0].session.some(
+      (i) => i === req.user.id
+    )
+    if (duplicateUser)
+      res.status(202).json({ info: 'You are in the room right now.' })
+    else
+      res.status(404).json({
+        info: 'Room is full. Please join other room or create new room.',
+      })
+  } else
+    res.status(404).json({
+      info: 'Room is full. Please join other room or create new room.',
+    })
 }
+
+//let gaming = {} // temporary storage
 
 const playGame = async (req, res) => {
   // TODO
@@ -97,6 +119,21 @@ const playGame = async (req, res) => {
   // show the result of each round
   // show the winner
   // finish
+  // const log = await db.Log.findOne({
+  //   where: {
+  //     room_id: req.params.room,
+  //     is_active: true,
+  //     //session: [req.user.id]
+  //   }
+  // })
+  // const
+  // let gaming = {}
+  // set gaming variable to empty again after result is shown
+  // USE MEMORY CACHE
+}
+
+const showResult = async (res, req) => {
+  // showResult
 }
 
 module.exports = {
@@ -104,5 +141,6 @@ module.exports = {
   getAllRooms,
   createRoom,
   fightRoom,
-  playGame
+  playGame,
+  showResult,
 }
--- routes/apiPlayers.js
+++ routes/apiPlayers.js
@@ -7,5 +7,8 @@ router.get('/players', restrict, controller.getAllPlayers)
 router.get('/rooms', restrict, controller.getAllRooms)
 router.post('/create-room', restrict, controller.createRoom)
 router.post('/fight/:room', restrict, controller.fightRoom)
+// extra features
+router.post('/fight/play/:room', restrict, controller.playGame)
+router.post('/fight/result/:room', restrict, controller.showResult)
 
 module.exports = router
